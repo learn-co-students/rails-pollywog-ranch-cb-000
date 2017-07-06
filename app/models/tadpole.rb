8830d9ac2358b340233ea0301fa40d52f1a054b6
jwald1 <email@example.com>
2017-07-06 15:14:14 -0400

    Done.

--- app/models/tadpole.rb
+++ app/models/tadpole.rb
@@ -2,4 +2,9 @@ class Tadpole < ActiveRecord::Base
   belongs_to :frog
 
   delegate :pond, to: :frog, allow_nil: true
+
+  def metamorphosize
+    tadpole = destroy
+    Frog.create(name: tadpole.name, color: tadpole.color, pond: tadpole.pond)
+  end
 end
